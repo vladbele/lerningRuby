c93bc20a0a4c795ddb926d4d19a12e2a180077b3
vladbele <49033624+vladbele@users.noreply.github.com>
2020-07-23 13:21:47 +0300

    endedApp

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles do
+    resources :comments
+  end
+
+  get 'article/new'
+
+  root 'welcome#index'
+end
